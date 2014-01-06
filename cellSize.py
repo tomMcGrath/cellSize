93124ad34e69ae3471afd127ccf5895ddb8355b5
tomMcGrath <thomas.m.mcgrath@gmail.com>
2014-01-06 16:12:12 +0000

    Added ecoli dataset, initial file handling code

---
+++ cellSize.py
@@ -0,0 +1,15 @@
+abundanceFile = open('', 'r')
+UniprotFile = open('', 'r')
+FASTAFile = open('', 'r')
+PSORTFile = open('', 'r')
+resultFile = open('results.txt', 'w')
+
+proteinDict = {}
+
+cellLife = 3000
+
+abundanceFile.close()
+UniprotFile.close()
+FASTAFile.close()
+PSORTFile.close()
+resultFile.close()
