9c679e9db919a51e6f603f67b208ef5a4a2c8888
tomMcGrath <thomas.m.mcgrath@gmail.com>
2014-01-06 17:00:45 +0000

    added localisation data

--- cellSize.py
+++ cellSize.py
@@ -12,7 +12,7 @@ cellLife = 3000 # seconds
 abundanceError = 0.0 # parts per million
 
 #Data dictionary
-proteinDict = {} # proteinDict: UniprotID -> [abundance, localisation, length, cost]
+proteinDict = {} # proteinDict: UniprotID -> [abundance, details, length, localisation, localisation certainty, multiple locations?]
 StringToUniprot = {} # StringToUniprot: StringID -> UniprotID (helps with abundance data, UniprotID is more widely used)
 
 for line in UniprotFile:
@@ -41,8 +41,39 @@ for datum in FASTAData[1:]:
     except KeyError:
         print header[0:6], 'is not 1-1 with Uniprot ID' # may happen v.rarely
         continue
-    
-        
+
+# Process localisation data from PSORT-B (needs updating for eukaryotes)
+PSORTFileContents = PSORTFile.read()
+PSORTData = PSORTFileContents.split('SeqID: sp|')
+
+for datum in PSORTData[1:]:
+    header = datum.partition('\n')[0][0:6]
+    headerData = datum.partition('\n')[0][6:]
+    analysis = datum.partition('\n')[2]
+
+    result = analysis.partition('Final Prediction:')
+    locationData = result[2].split('\n')[1]
+
+    if locationData.find('Unknown') != -1:
+        location = 'Unknown'
+        probability = 10.0
+        possMany = False
+
+    else:
+        location = locationData.split(' ')[4]
+        probability = locationData.split(' ')[-1][:-1]
+        if locationData.split(' ')[5] != '':
+            possMany = True
+        else:
+            possMany = False
+    try:
+        proteinDict[header].append(location)
+        proteinDict[header].append(float(probability))
+        proteinDict[header].append(possMany)
+
+    except KeyError:
+        print header[0:6], 'is not 1-1 with Uniprot ID' # may happen v.rarely
+        continue
 
 abundanceFile.close()
 UniprotFile.close()
