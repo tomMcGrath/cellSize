2d4ab897917a1403a582bb423ef18251559576a9
tomMcGrath <thomas.m.mcgrath@gmail.com>
2014-01-06 16:25:10 +0000

    added abundance data

--- cellSize.py
+++ cellSize.py
@@ -1,12 +1,32 @@
-abundanceFile = open('', 'r')
-UniprotFile = open('', 'r')
-FASTAFile = open('', 'r')
-PSORTFile = open('', 'r')
+# Data files
+abundanceFile = open('511145-E.coli_whole_organism-integrated_dataset.txt', 'r')
+UniprotFile = open('2013121951YKSHD5B0.tab', 'r')
+FASTAFile = open('ecoli_sequences.fasta', 'r')
+PSORTFile = open('psort.txt', 'r')
+
+# Output files
 resultFile = open('results.txt', 'w')
 
-proteinDict = {}
+#Global variables
+cellLife = 3000 # seconds
+abundanceError = 0.0 # parts per million
 
-cellLife = 3000
+#Data dictionary
+proteinDict = {} # proteinDict: UniprotID -> [abundance, localisation, length, cost]
+StringToUniprot = {}
+
+for line in UniprotFile:
+    data = line.split('\t')
+    StringToUniprot[data[0]] = data[1][:-1]
+
+for line in abundanceFile:
+    if line[0] != '#':
+        data = line.split('\t')
+        try:
+            proteinDict[StringToUniprot[data[1]]] = float(data[2][:-1])
+        except KeyError:
+            abundanceError += float(data[2])
+        
 
 abundanceFile.close()
 UniprotFile.close()
